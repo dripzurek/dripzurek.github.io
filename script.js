5076ecfac4999f55c34ebd40f089f456a59b8d0e
dripzurek <150512000+dripzurek@users.noreply.github.com>
2023-12-28 23:53:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ async function getRepoContents(path) {
     try {
         const response = await fetch(`https://api.github.com/repos/dripzurek/dripzurek.github.io/contents/${path}`);
         const data = await response.json();
-        console.log('Respuesta de la API:', data);
         showContents(data, path);
     } catch (error) {
         console.error('Error al obtener la lista de archivos:', error);
@@ -22,36 +21,34 @@ function showContents(contents, path) {
     fileList.innerHTML = '';
 
     contents.forEach(item => {
-        // Convertir el nombre del archivo a minúsculas para evitar problemas de mayúsculas/minúsculas
-        const itemNameLowerCase = item.name.toLowerCase();
-
         // Omitir archivos específicos
-        if (itemNameLowerCase === 'index.html' || itemNameLowerCase === 'script.js' || itemNameLowerCase === 'styles.css') {
-            const listItem = document.createElement('li');
-            const link = document.createElement('a');
-
-            if (item.type === 'file') {
-                link.href = item.download_url;
-                link.setAttribute('download', item.name);
-            } else if (item.type === 'dir') {
-                link.classList.add('folder-icon');
-                link.href = '#';
-                link.addEventListener('click', (event) => {
-                    event.preventDefault();
-                    historyStack.push(path);
-                    updateUrl(item.path);
-                    getRepoContents(item.path);
-                });
-            }
-
-            link.textContent = item.name;
-            listItem.appendChild(link);
-            fileList.appendChild(listItem);
+        if (['index.html', 'styles.css', 'script.js'].includes(item.name)) {
+            return;
         }
+
+        const listItem = document.createElement('li');
+        const link = document.createElement('a');
+
+        if (item.type === 'file') {
+            link.href = item.download_url;
+            link.setAttribute('download', item.name);
+        } else if (item.type === 'dir') {
+            link.classList.add('folder-icon');
+            link.href = '#';
+            link.addEventListener('click', (event) => {
+                event.preventDefault();
+                historyStack.push(path);
+                updateUrl(item.path);
+                getRepoContents(item.path);
+            });
+        }
+
+        link.textContent = item.name;
+        listItem.appendChild(link);
+        fileList.appendChild(listItem);
     });
 
-    // Mostrar u ocultar el botón "Inicio" según si estás en el inicio o dentro de una carpeta
-    navButtons.style.display = historyStack.length > 0 ? 'flex' : 'none';
+    navButtons.style.display = path === '' ? 'none' : 'flex';
 }
 
 function goToHome() {
@@ -60,10 +57,6 @@ function goToHome() {
         updateUrl(previousPath);
         getRepoContents(previousPath);
     } else {
-        const currentPath = location.search.replace('?path=', '');
-        if (currentPath !== '') {
-            historyStack.push(currentPath);
-        }
         updateUrl('');
         getRepoContents('');
     }
@@ -73,5 +66,5 @@ function updateUrl(path) {
     history.pushState({ path: path }, null, path ? `?path=${path}` : window.location.pathname);
 }
 
-// Cargar contenidos iniciales
+// Cargar contenido inicial
 getRepoContents('');
