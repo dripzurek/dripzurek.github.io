318fd0f21f3f5fec456f218b3687621e468d58ac
dripzurek <150512000+dripzurek@users.noreply.github.com>
2023-12-29 01:12:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,109 +1,69 @@
-// URL del repositorio en GitHub
-const repoUrl = 'https://api.github.com/repos/DirectoryLister/DirectoryLister/contents/';
-
-// Elemento del botón de navegación
 const navButtons = document.getElementById('nav-buttons');
-
-// Elemento de la lista de archivos
 const fileList = document.getElementById('file-list');
-
-// Pila de historial para gestionar la navegación
 const historyStack = [];
 
-// Manejar el evento 'popstate' (cambio en el historial)
-window.addEventListener('popstate', handlePopState);
+window.addEventListener('popstate', (event) => {
+    const path = event.state ? event.state.path : '';
+    getRepoContents(path);
+});
 
-// Función asincrónica para obtener el contenido del repositorio
 async function getRepoContents(path) {
     try {
-        const response = await fetch(`${repoUrl}${path}`);
-        if (!response.ok) throw new Error(`Error: ${response.statusText}`);
-        
+        const response = await fetch(`https://api.github.com/repos/DirectoryLister/DirectoryLister/contents/${path}`);
         const data = await response.json();
         showContents(data, path);
     } catch (error) {
-        console.error('Error al obtener la lista de archivos:', error.message);
+        console.error('Error al obtener la lista de archivos:', error);
     }
 }
 
-// Mostrar el contenido en la interfaz
 function showContents(contents, path) {
-    clearFileList();
-    
-    contents.forEach(item => {
-        if (isExcludedFile(item.name)) return;
+    fileList.innerHTML = '';
 
-        const listItem = createListItem(item);
+    contents.forEach(item => {
+        // Omitir archivos específicos
+        if (['index.html', 'styles.css', 'script.js'].includes(item.name)) {
+            return;
+        }
+
+        const listItem = document.createElement('li');
+        const link = document.createElement('a');
+
+        if (item.type === 'file') {
+            link.href = item.download_url;
+            link.setAttribute('download', item.name);
+        } else if (item.type === 'dir') {
+            link.classList.add('folder-icon');
+            link.href = '#';
+            link.addEventListener('click', (event) => {
+                event.preventDefault();
+                historyStack.push(path);
+                updateUrl(item.path);
+                getRepoContents(item.path);
+            });
+        }
+
+        link.textContent = item.name;
+        listItem.appendChild(link);
         fileList.appendChild(listItem);
     });
 
-    updateNavButtonsVisibility(path);
-}
-
-// Crear un elemento de lista para un archivo o carpeta
-function createListItem(item) {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-
-    if (item.type === 'file') {
-        setupFileLink(link, item);
-    } else if (item.type === 'dir') {
-        setupFolderLink(link, item);
-    }
-
-    link.textContent = item.name;
-    listItem.appendChild(link);
-    return listItem;
-}
-
-// Configurar un enlace para un archivo
-function setupFileLink(link, item) {
-    link.href = item.download_url;
-    link.setAttribute('download', item.name);
-}
-
-// Configurar un enlace para una carpeta
-function setupFolderLink(link, item) {
-    link.classList.add('folder-icon');
-    link.href = '#';
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-        handleFolderClick(item.path);
-    });
-}
-
-// Manejar el clic en una carpeta
-function handleFolderClick(path) {
-    historyStack.push(history.state ? history.state.path : '');
-    updateUrl(path);
-    getRepoContents(path);
-}
-
-// Verificar si un archivo debe excluirse de la lista
-function isExcludedFile(fileName) {
-    return ['index.html', 'styles.css', 'script.js'].includes(fileName);
-}
-
-// Actualizar la visibilidad de los botones de navegación
-function updateNavButtonsVisibility(path) {
     navButtons.style.display = path === '' ? 'none' : 'flex';
 }
 
-// Limpiar la lista de archivos
-function clearFileList() {
-    fileList.innerHTML = '';
+function goToHome() {
+    if (historyStack.length > 0) {
+        const previousPath = historyStack.pop();
+        updateUrl(previousPath);
+        getRepoContents(previousPath);
+    } else {
+        updateUrl('');
+        getRepoContents('');
+    }
 }
 
-// Manejar cambios en el historial de navegación
-function handlePopState(event) {
-    const path = event.state ? event.state.path : '';
-    getRepoContents(path);
-}
-
-// Actualizar la URL y el historial de navegación
 function updateUrl(path) {
-    const newPath = path ? `?path=${path}` : window.location.pathname;
-    history.pushState({ path: path }, null, newPath);
+    history.pushState({ path: path }, null, path ? `?path=${path}` : window.location.pathname);
 }
 
 // Cargar contenido inicial
