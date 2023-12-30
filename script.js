b925bf371922bf9a3a9fdf900c6109b4bee9a19e
dripzurek <150512000+dripzurek@users.noreply.github.com>
2023-12-30 13:36:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -36,6 +36,8 @@ function showContents(contents, path) {
             // Configurar el enlace según el tipo de archivo
             if (item.type === 'file') {
                 setupFileLink(link, item);
+                // Agregar tamaño y hora de modificación al título del enlace
+                link.title = `Size: ${formatFileSize(item.size)} | Last Modified: ${formatDate(item.last_modified)}`;
             } else if (item.type === 'dir') {
                 setupFolderLink(link, item, path);
             }
